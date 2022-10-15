a1c64622dd024c62e6a5d57546fa8bffd02e4b9a
USER <USER@DESKTOP-TRQG01D>
2022-10-15 13:54:41 +0300

    Добавьте файлы проекта.

---
+++ Pr.8.Zd.4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение переменной а: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите значение переменной b: ");
+            int b = int.Parse(Console.ReadLine());
+            if (a > b) Console.WriteLine("Ошибка! Переменная а > b");
+            else
+            {
+                int i, sum = 0;
+                for (i = a; b >= i; i++)
+                    sum += i;
+                Console.WriteLine("Сумма чисел равна: " + sum);
+            }
+            Console.ReadLine();
+        }
+    }
+}
